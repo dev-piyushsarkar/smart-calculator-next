d9349b89669eff394ce516fa1e8c8dac61de35cb
Piyush Sarkar <piyushsarkar.dev@gmail.com>
2025-12-27 13:25:42 +0530

    Add  UI Select component and update dependencies

--- src/components/SmartCalculator.tsx
+++ src/components/SmartCalculator.tsx
@@ -1,12 +1,38 @@
+import { Divide, MinusIcon, PlusIcon, X } from "lucide-react";
 import { Button } from "./shadcnui/button";
 import { Input } from "./shadcnui/input";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "./shadcnui/select";
 
 const SmartCalculator = () => {
 	return (
 		<section className="grid grid-cols-3 gap-5">
 			<Input placeholder="Number 1" />
+			<Select>
+				<SelectTrigger className="w-full">
+					<SelectValue placeholder="Operators"></SelectValue>
+				</SelectTrigger>
+				<SelectContent>
+					<SelectItem value="plus">
+						<PlusIcon />
+					</SelectItem>
+					<SelectItem value="minus">
+						<MinusIcon />
+					</SelectItem>
+					<SelectItem value="multiply">
+						<X />
+					</SelectItem>
+					<SelectItem value="divide">
+						<Divide />
+					</SelectItem>
+				</SelectContent>
+			</Select>
 			<Input placeholder="Number 2" />
-			<Input />
 			<Input
 				disabled
 				placeholder="Total"
